9f86119def4aeb963ccec8c40a1b0dce499efdc8
SakilaShrestha06 <shresthasakila0663@gmail.com>
2025-11-05 10:09:22 +0545

    Initial commit with Task 1 and project files

---
+++ Task 1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string userName = "Sakila";
+
+Console.WriteLine("Hello, "+userName);
+
+int luckyNumber = 4;
+
+Console.WriteLine("LuckyNumber, "+luckyNumber);
+
+String message = $"Hello, {userName}! Your lucky number is {luckyNumber}.";
+Console.WriteLine(message);
